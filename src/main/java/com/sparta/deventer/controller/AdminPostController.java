a736c1feefa9d7b0f8749800fa7d026e03cc17e2
seokhyeonho <162691839+seokhyeonho@users.noreply.github.com>
2024-06-25 11:00:43 +0900

    Refactor: 공지글 생성 메서드 수정

--- src/main/java/com/sparta/deventer/controller/AdminPostController.java
+++ src/main/java/com/sparta/deventer/controller/AdminPostController.java
@@ -11,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,7 +25,7 @@ public class AdminPostController {
     private final AdminPostService adminPostService;
 
     // 공지글 생성
-    @PutMapping("/posts/notice")
+    @PostMapping("/posts/notice")
     public ResponseEntity<PostResponseDto> createNoticePost(
             @RequestBody PostRequestDto postRequestDto,
             @AuthenticationPrincipal UserDetailsImpl userDetails) {
